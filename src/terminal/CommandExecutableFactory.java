e66e6d45e53e2ea1260147c2e6a0f07701073d25
Ilya Tsibul <tsigel@vikivostok.ru>
2023-02-25 04:08:18 +0300

    changed to switch

--- src/terminal/CommandExecutableFactory.java
+++ src/terminal/CommandExecutableFactory.java
@@ -12,28 +12,34 @@ public class CommandExecutableFactory {
         this.zoo = zoo;
     }
 
-    public void setZoo(Zoo zoo) {
-        this.zoo = zoo;
-    }
-
     public CommandExecutable create(String[] input) {
         String code = input[0] + input[1];
-        if (code.equals("wolfdelete")) return new DeleteWolfExecutable(zoo);
-        else if (code.equals("liondelete")) return new DeleteLionExecutable(zoo);
-        else if (code.equals("snakedelete")) return new DeleteSnakeExecutable(zoo);
-        else if (code.equals("lioncreate")){
-            Lion lion = new Lion(input[2], Integer.parseInt(input[3]), Integer.parseInt(input[4]), 4, Integer.parseInt(input[5]));
-            return new CreateLionExecutable(zoo, lion);
+        switch (code) {
+            case "wolfdelete" -> {
+                return new DeleteWolfExecutable(zoo);
+            }
+            case "liondelete" -> {
+                return new DeleteLionExecutable(zoo);
+            }
+            case "snakedelete" -> {
+                return new DeleteSnakeExecutable(zoo);
+            }
+            case "lioncreate" -> {
+                Lion lion = new Lion(input[2], Integer.parseInt(input[3]), Integer.parseInt(input[4]), 4, Integer.parseInt(input[5]));
+                return new CreateLionExecutable(zoo, lion);
+            }
+            case "wolfcreate" -> {
+                Wolf wolf = new Wolf(input[2], Integer.parseInt(input[3]), Integer.parseInt(input[4]), 4, Integer.parseInt(input[5]));
+                return new CreateWolfExecutable(zoo, wolf);
+            }
+            case "snakecreate" -> {
+                Snake snake = new Snake(input[2], Integer.parseInt(input[3]), Integer.parseInt(input[4]), 4, Integer.parseInt(input[5]));
+                return new CreateSnakeExecutable(zoo, snake);
+            }
+            default -> {
+                return null;
+            }
         }
-        else if (code.equals("wolfcreate")){
-            Wolf wolf = new Wolf(input[2], Integer.parseInt(input[3]), Integer.parseInt(input[4]), 4, Integer.parseInt(input[5]));
-            return new CreateWolfExecutable(zoo, wolf);
-        }
-        else if (code.equals("snakecreate")){
-            Snake snake = new Snake(input[2], Integer.parseInt(input[3]), Integer.parseInt(input[4]), 4, Integer.parseInt(input[5]));
-            return new CreateSnakeExecutable(zoo, snake);
-        }
-        else return null;
     }
 
 }
