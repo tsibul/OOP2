5fae2d3183141972e8e84faa3d0d70a436236157
Ilya Tsibul <tsigel@vikivostok.ru>
2023-03-02 23:42:54 +0300

    format

--- src/terminal/message/InviteMessage.java
+++ src/terminal/message/InviteMessage.java
@@ -4,8 +4,8 @@ public class InviteMessage extends Message{
 
     public InviteMessage() {
         this.message = "input what you want\n " +
-                "Format: <type of animal> delete or\n " +
-                "<type of animal> create name year_of_birth weight extra_number" +
+                "Format:\n <type of animal> delete or\n " +
+                "<type of animal> create\nthen: name year_of_birth weight extra_number" +
                 "(maneVolume, height, bodyLength etc.)";
     }
 }
